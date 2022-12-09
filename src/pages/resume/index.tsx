2a8f9e7236df87cbb677195b25e09f35e5ced5d1
JD Ghuman <jdshguman@gmail.com>
2022-12-09 12:58:29 -0600

    Update resume meta tags

--- src/pages/resume/index.tsx
+++ src/pages/resume/index.tsx
@@ -2,12 +2,24 @@ import Link from "next/link";
 import ImageDisplay from "@components/ImageDisplay";
 import { AiOutlineDownload } from "react-icons/ai";
 import Button from "@components/Button";
+import Meta from "@components/Meta";
+import { MetaTags, PageType, RobotsContent } from "@components/Meta/types";
 
 import styles from "./index.module.scss";
 
 const Resume = () => {
+  const postMetaTags: MetaTags = {
+    canonical: "https://www.bobbieleeghuman.com",
+    description: `UX Designer | Portfolio. Language Learning App`,
+    image: "https://www.bobbieleeghuman.com/images/bobbie.png",
+    robots: `${RobotsContent.follow},${RobotsContent.index}`,
+    title: `bobbieleeghuman | Resume`,
+    type: PageType.website,
+  };
+
   return (
     <div className={styles.container}>
+      <Meta tags={postMetaTags} />
       <div>
         <Link target="_blank" href="/resume.pdf">
           <Button className={styles.button} primary>
